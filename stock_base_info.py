9626e21fc4d4643f96d7db129e866621807a42eb
tgao <gaotianpu@gmail.com>
2013-09-26 18:46:39 +0800

    first init data from ifeng
    first init data from ifeng

--- stock_base_info.py
+++ stock_base_info.py
@@ -45,16 +45,51 @@ def parse_html(lfile):
           thestrings = [unicode(s) for s in col.findAll(text=True)]
           thetext = ''.join(thestrings)
           result[-1].append(thetext)
-
+    
     return result 
     
-def download_all_stocks():
+def download_all_stocks(func):
     t_li = const_tmarkets.split(',')
     for t in t_li:
-        for p in range(1,10):
+        for p in range(1,1000):
             fname = download({'t':t,'p':p,'f':'symbol','o':'asc'})
-            parse_html(fname)
+            results = parse_html(fname)
+            if len(results)<3:
+                print t + " finished " + str(p) 
+                break            
+            func(t,results)
+
+####
+dbw = web.database(dbn='mysql', host='127.0.0.1', db='forecast', user='root', pw='root')
+dbr = web.database(dbn='mysql', host='127.0.0.1', db='forecast', user='root', pw='root')
+
+def import_to_db(t,results):
+    stock_nos = load_all_stock_nos()
+    l = []
+    for r in results:
+        if len(r)<7:
+            print "len less 7 - " + str(r)
+            if len(r)>0 and u'下一页' not in r[0]:
+                print t+" finished"
+            continue
+        if r[0] not in stock_nos:
+            row = {'market_code':t,'stock_no':r[0],'stock_name':r[1],'create_date':datetime.datetime.now(),'last_update':datetime.datetime.now()}
+            print row
+            l.append(row)
+        else:
+            print r[0]+' exist in db'
+
+    dbw.supports_multiple_insert = True
+    dbw.multiple_insert('stock_base_infos',l)
+
+
+
+def load_all_stock_nos():
+    results = dbr.select('stock_base_infos',what='stock_no')
+    l = [r.stock_no for r in results]
+    return l
 
 
 if __name__ == '__main__':
-    download_all_stocks()
+    download_all_stocks(import_to_db)
+
