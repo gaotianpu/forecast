ce93bbc974209fad4983cdc2ee66238add3f82fb
tgao <gaotianpu@gmail.com>
2013-09-26 18:51:59 +0800

    backup sql structure
    backup

---
+++ forecast.sql
@@ -0,0 +1,70 @@
+-- MySQL dump 10.13  Distrib 5.5.27, for osx10.6 (i386)
+--
+-- Host: localhost    Database: forecast
+-- ------------------------------------------------------
+-- Server version	5.5.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `stock_base_infos`
+--
+
+DROP TABLE IF EXISTS `stock_base_infos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `stock_base_infos` (
+  `pki_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `market_code` varchar(5) DEFAULT NULL,
+  `stock_no` varchar(10) DEFAULT NULL,
+  `stock_name` varchar(20) DEFAULT NULL,
+  `create_date` datetime DEFAULT NULL,
+  `last_update` datetime DEFAULT NULL,
+  PRIMARY KEY (`pki_id`),
+  UNIQUE KEY `stock_no` (`stock_no`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `stock_daily_records`
+--
+
+DROP TABLE IF EXISTS `stock_daily_records`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `stock_daily_records` (
+  `pk_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `date` date DEFAULT NULL,
+  `stock_market_id` int(11) DEFAULT NULL,
+  `stock_id` int(11) DEFAULT NULL,
+  `open_price` decimal(8,2) DEFAULT NULL,
+  `high_price` decimal(8,2) DEFAULT NULL,
+  `low_price` decimal(8,2) DEFAULT NULL,
+  `close_price` decimal(8,2) DEFAULT NULL,
+  `volume` int(11) DEFAULT NULL,
+  `adj_close` decimal(8,2) DEFAULT NULL,
+  `create_date` datetime DEFAULT NULL,
+  `last_update` datetime DEFAULT NULL,
+  PRIMARY KEY (`pk_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-09-26 18:51:06
---
+++ test.py
@@ -0,0 +1,1 @@
+#/usr/local/mysql/bin/mysqldump -uroot -proot -d forecast > /Users/gaotianpu/github/forecast/forecast.sql
