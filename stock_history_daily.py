9782c4c4576e2173b21becb0742d8034c338dc9d
tgao <gaotianpu@gmail.com>
2013-10-12 16:21:47 +0800

    clean codes
    clean codes

--- stock_history_daily.py
+++ stock_history_daily.py
@@ -1,37 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import web
-import urllib 
-import urllib2
-import random
+from config import dbr,dbw,const_root_local,init_log
+import browser
 import datetime
 import os
 import csv
-from config import dbr,dbw,const_root_local,init_log
-import da
-import browser
 
-#doc:http://blog.t41.cn/index.php/archives/254 
+#doc:http://www.bizeway.net/read.php?317 
 #http://table.finance.yahoo.com/table.csv?s=000001.sz
 
 loger = init_log("history_daily")
-
-
-const_root_url = "http://table.finance.yahoo.com/table.csv?" # "http://ichart.yahoo.com/table.csv?"
-
-params={'s':'600000.SS','a':'00','b':'01','c':2013,'d':'09','e':'01','f':'2013','g':'d'}
-mcodes = {'sa':'sz','sb':'sz','ha':'ss','hb':'ss','zs':'','ss':'ss','sz':'sz'}
+const_root_url = "http://table.finance.yahoo.com/table.csv?" 
 
 def get_url(params):
     return '%ss=%s' % (const_root_url,params['s'])
-    return '%ss=%s&a=%s&b=%s&c=%s&d=%s&e=%s&f=%s&g=%s' % (const_root_url,params['s'],params['a'],params['b'],params['c'],params['d'],params['e'],params['f'],params['g'])
-    return const_root_url + '&'.join(["%s=%s" % (k,v) for k,v in params.items()])
 
 def get_local_file_name(params): 
-    return '%s/dailyh/%s.csv' %(const_root_local,params['s'])   
-    local_file = '%s_%s%s%s_%s%s%s_%s.csv' % (params['s'],params['c'],params['b'],params['a'],params['f'],params['e'],params['d'],params['g'])
-    lfile = '%s/dailyh/%s' %(const_root_local,local_file)
-    return lfile
+    return '%s/dailyh/%s.csv' %(const_root_local,params['s'])
+    #return lfile
 
 def parse_data(lfile):
     l=[]
@@ -81,6 +68,7 @@ def import_stock_daily_data(market_code,stock_no,data):
 def download_all(stocks):        
     for s in stocks:
         scode = '%s.%s' % (s.stock_no,s.market_code_yahoo)
+        #params={'s':scode}
         params={'s':scode,'a':'00','b':'01','c':2013,'d':'9','e':'01','f':'2013','g':'d'}  
         lfile = get_local_file_name(params)
         url = get_url(params)  
@@ -92,7 +80,7 @@ def download_all(stocks):
             data = parse_data(lfile)
             import_stock_daily_data(s.market_code,s.stock_no,data)
         except Except,e:
-            loger.err(" except "+ e)
+            loger.err(" except "+ e + url)
             
 
 def load_failed_stock():
