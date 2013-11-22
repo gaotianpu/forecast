9c63327bc81168f07551291445c5043396b1f04c
Thomas Gao <gaotianpu@gmail.com>
2013-11-22 15:26:21 +0800

    3,5 days price_rate, trend
    cc

--- last_3_5.py
+++ last_3_5.py
@@ -7,6 +7,22 @@ import da
 def update(stock_no,result):
     pass
 
+def compute_trend(rows):
+    rows = sorted(rows, cmp=lambda x,y : cmp(x.close_price, y.close_price))
+
+    i=1
+    for r in rows:
+        r.index = i
+        i = i + 1
+
+    t=''
+    rows = sorted(rows, cmp=lambda x,y : cmp(x.date, y.date))
+    for r in rows:
+        t = t + str(r.index)
+
+    return int(t)
+
+
 def compute_5_3(stock_trade_records_5):
     days_count = len(stock_trade_records_5)
 
@@ -18,11 +34,13 @@ def compute_5_3(stock_trade_records_5):
     day_3_price_rate = (day_0.close_price - day_3.open_price)/day_3.open_price
     day_5_price_rate = (day_0.close_price - day_5.open_price)/day_5.open_price
 
-    #3日内最大涨幅\走势
-    #5日内最大涨幅\走势
-    print day_3_price_rate,day_5_price_rate
+    #趋势线
+    trend_3 = compute_trend(stock_trade_records_5[0:3] if days_count>3 else stock_trade_records_5)
+    trend_5 = compute_trend(stock_trade_records_5)
 
-    return web.storage(days_count=days_count,day_3_price_rate=day_3_price_rate,day_5_price_rate=day_5_price_rate)
+    #print trend_3,day_3_price_rate,trend_5,day_5_price_rate
+    return web.storage(days_count=days_count,trend_5=trend_5,trend_3=trend_3,
+        day_3_price_rate=day_3_price_rate,day_5_price_rate=day_5_price_rate)
 
 def run():
     trade_records_5 = da.dailyrecords.load_all_last_5()
