6b4b69d09452bb87de69dd7f8a86124049663ee8
Thomas Gao <gaotianpu@gmail.com>
2013-11-27 13:55:25 +0800

    ccc
    ccc

---
+++ util/emailsmtp.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''it's a python script to get the pv,then send email to managers.'''
+import smtplib,time
+from email.MIMEText import MIMEText
+from email.MIMEMultipart import MIMEMultipart
+from datetime import datetime,date,timedelta
+
+def sendmail(subject,content,tousrs):
+
+
+    msg = MIMEText(content,'html','utf-8')
+    msg['Subject']= subject
+    msg['To'] = ','.join(tousrs)
+    server = smtplib.SMTP(hostname)
+    server.set_debuglevel(1)
+    server.login(from_usr, from_usr_pwd)
+    server.sendmail(from_usr, tousrs, msg.as_string())
+    server.quit()
+
+def sendmail_attach(subject,content,tousrs,attchfiles):
+    msg = MIMEMultipart()
+    txt = MIMEText(content,'html','utf-8')
+    msg.attach(txt)
+    for attchfile in attchfiles:
+        att = MIMEText(open(attchfile,'rb').read(),'base64','utf-8')
+        att["Content-Type"] = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        att["Content-Disposition"] = 'attachment;filename="%s"' %(os.path.basename(attchfile))
+        msg.attach(att)
+
+    msg['Subject']= subject
+    msg['To'] ='tgao@airizu.com'
+    server = smtplib.SMTP(hostname)
+    server.set_debuglevel(1)
+    server.login(from_usr, from_usr_pwd)
+    server.sendmail(from_usr, tousrs, msg.as_string())
+    server.quit()
+
+if __name__ == "__main__":
+    sendmail('helloword','test','aa@qq.com')
