6ec9aa3604b34cea1da3d5aa9f3d3b74f7bf1f82
Thomas Gao <gaotianpu@gmail.com>
2014-01-03 21:27:51 +0800

    add query ui
    cc

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import web
+from config import dbr,dbw,const_root_local,init_log
+import comm
 
-urls = ('cyb',Cyb)
+urls = ('/','Index')
+render = web.template.render('templates')
 
-class Cyb:
-     def GET(self):
-     	render = web.template.render('templates/cyb')
-     	return 
+class Index:
+    def GET(self):
+    	r = web.storage(stocks=[],query='',count=0) 
+     	return render.index(r)
+    def POST(self):
+    	i = web.input(sql="")
+    	rows = list(dbr.select('stock_daily',
+    		where="trade_date='2014-1-3' and volume<>0 and (" + i.sql + ")",
+    		vars=locals()))
+
+    	l = []
+    	for r in rows:
+    		code = comm.get_market_codes(r.stock_no)
+        	if not code['pinyin'] : continue
+        	l.append(web.storage(stock_no=r.stock_no,pinyin2=code['pinyin']) )    		 
+
+    	r = web.storage(stocks=l,query=i.sql,count = len(l)) 
+     	return render.index(r)
+
+app = web.application(urls, globals())
+
+if __name__ == "__main__":
+    app.run()
