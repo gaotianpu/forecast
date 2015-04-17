3dc75dcea3d9a4f6a993d6a1e6fbb95a3a1eb825
tgao <gaotianpu@gmail.com>
2015-04-17 22:23:18 +0800

    update smtp config
    ccc

--- v2/config.py
+++ v2/config.py
@@ -4,16 +4,24 @@ import datetime
 
 #定义一些配置量
 #
+
+is_dev = 1 
+
+#local dirs
 stocks_list_file = "/Users/gaotianpu/github/forecast/v2/all_stocks_list.txt"  #存放所有stock列表
 local_root_dir = "/Users/gaotianpu/Documents/stocks/"  #数据下载存储位置
 history_data_dir = local_root_dir + "history/"  #单只stock历史数据存放位置
 daily_data_dir = local_root_dir + "daily/" #每天所有stocks数据存放目录
 history_ma_dir = local_root_dir + "ma/"  #单只stock历史数据存放位置
-
 history_price_change_rate_dir = local_root_dir + "history_price_change_rate/" #每天所有stocks数据存放目录
 latest_data_dir = local_root_dir + "latest/" #
 
-def get_today():
-    today = datetime.datetime.now()
-    trade_date = today.strftime('%Y-%m-%d')
-    return trade_date
+
+#smtp
+smtp_host = "smtp.163.com"
+smtp_usr = "xxxxx"
+smtp_pass = 'xxxxx'
+smtp_from = 'xxxxxx@163.com'
+smtp_mailto =['xxxxxxxxx@qq.com']
+#
+
