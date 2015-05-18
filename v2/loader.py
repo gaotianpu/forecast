7de0b93fdc258a9c574e9fca7f53d989ee739c6b
tgao <gaotianpu@gmail.com>
2015-05-18 18:06:22 +0800

    improve code
    cc

--- v2/loader.py
+++ v2/loader.py
@@ -21,8 +21,63 @@ def load_stock_history(stock_no):
             r = web.storage(date=items[0],open=float(items[1]),high=float(items[2]),
                 low=float(items[3]),close=float(items[4]),
                 volume=int(items[5]),adjclose=float(items[6]),amount=0,last_close=0)                                        
-            records.append(r)            
+            records.append(r)  
+            # print r          
     return records
+
+def extend_history_data(stock_no):
+    compute = lambda records,days : '%.2f' % numpy.array([x.adjclose for x in records[i:i+days]]).mean() 
+    cvolume = lambda records,days : '%.0f' % numpy.array([x.volume for x in records[i:i+days]]).mean() 
+
+    records = load_stock_history(stock_no)    
+    
+    l = []    
+    
+    count  = len(records)
+    for i in range(0,count): 
+        r = records[i] 
+
+        last_adj_close = 0
+        today_prate = 0
+        jump = 0 
+        is_raise = 0
+        tommorrow_prate = 0 
+        if i<count-1:
+            last_adj_close = records[i+1].adjclose
+            today_prate = (r.adjclose - last_adj_close)*100/ last_adj_close 
+            if today_prate > 3:
+                is_raise = 1 
+            jump = (r.open - last_adj_close)*100/ last_adj_close  
+            if i>0:
+                tommorrow_prate = ( records[i-1].adjclose - r.adjclose)*100/ r.adjclose   # ( - r.adjclose)*100 / r.adjclose) 
+        
+        item = web.storage(
+            date = r.date,
+            last_adjclose = last_adj_close,
+
+            tommorrow_prate = tommorrow_prate,
+            today_prate = today_prate,
+
+            ma5 = compute(records,5),
+            ma10 = compute(records,10) ,  
+            ma30 = compute(records,30)  , 
+            ma60 =  compute(records,60) ,
+            ma120 = compute(records,120) , 
+            ma240 = compute(records,240)  ,
+
+            v5 = cvolume(records,5),
+            v10 = cvolume(records,10),   
+            v30 = cvolume(records,30) ,  
+            v60 =  cvolume(records,60) ,
+            v120 = cvolume(records,120) , 
+            v240 = cvolume(records,240) 
+
+            )
+        print r.date,last_adj_close,r.adjclose,records[i-1].adjclose,jump,today_prate,tommorrow_prate
+        # if i >30: break
+        l.append(item)  
+
+    return l
 
 
 def load_daily_stocks(date):       
@@ -81,6 +136,7 @@ def comput_ma(stock_no):
     cvolume = lambda records,days : '%.0f' % numpy.array([x.volume for x in records[i:i+days]]).mean() 
     
     l = []
+    l_jump = []
     records = load_stock_history(stock_no)
     count  = len(records)
     for i in range(0,count): 
@@ -110,6 +166,8 @@ def comput_ma(stock_no):
         v60 =  cvolume(records,60) 
         v120 = cvolume(records,120)  
         v240 = cvolume(records,240)  
+
+        l_jump.append(web.storage(date=r.date,jump=jump, lcc=prate ))  #(r.date,jump,prate))
                
         l.append((r.date,str(is_raise),str(prate),str(jump),str(last_adj_close),ma5,ma10,ma30,ma60,ma120,ma240,v5,v10,v30,v60,v120,v240))
 
@@ -206,8 +264,8 @@ def all_volume():
 
     
 if __name__ == "__main__" :      
-    # load_stock_history('600000.ss')
-    load_daily_stocks_v2('2015-04-13')    
+    extend_history_data('600000.ss')
+       
     # price_change_rate('600000.ss',3)
     # all_price_change_rate()
     # merge_history_and_today('2015-04-07')
