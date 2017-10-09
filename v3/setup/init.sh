d3a3780f1fe4868ab52172c6fa72532d2eca5b82
tgao <gaotianpu@gmail.com>
2017-10-09 21:55:06 +0800

    cc

--- v3/setup/init.sh
+++ v3/setup/init.sh
@@ -44,22 +44,28 @@ function create_db_schema(){
     sqlite3 $SQLITE3_DB_FILE < $PROJECT_ROOT/setup/schema.sql || exit 
 } 
 
-function init(){
-    make_dirs 
-    create_db_schema   
-
-    echo "import stocks"  
-    grep -v 'stock_no' $ALL_STOCKS_FILE > $ALL_STOCKS_WITHOUT_HEADER_FILE     
-    time sqlite3  -separator ',' -header $SQLITE3_DB_FILE ".import $ALL_STOCKS_WITHOUT_HEADER_FILE stocks"  
-    
+function download_convert(){
     if [ ! -f $ALL_HISTORY_FILE ];then
         echo "convert trade records" 
         time $PROJECT_ROOT/bin/history_data.py || exit  # > $HISTORY_DATA_PATH/all.csv  || exit 
         echo "merge trade records" 
+
         cd $HISTORY_CONVERTED_PATH
         time cat * > $ALL_HISTORY_FILE || exit 
     fi 
+}
 
+function init(){
+    make_dirs  
+
+    download_convert
+
+    create_db_schema   
+
+    echo "import stocks"  
+    grep -v 'stock_no' $ALL_STOCKS_FILE > $ALL_STOCKS_WITHOUT_HEADER_FILE     
+    time sqlite3  -separator ',' $SQLITE3_DB_FILE ".import $ALL_STOCKS_WITHOUT_HEADER_FILE stocks"  
+     
     echo "import stocks trade records" 
     time sqlite3  -separator ',' $SQLITE3_DB_FILE  ".import $ALL_HISTORY_FILE stocks_trade_records" || exit 
 
@@ -85,9 +91,7 @@ function make_lable(){
 #   None
 ####################################### 
 function main(){
-    init
-
-    return
+    # init 
 
     make_lable 1 
     make_lable 2
