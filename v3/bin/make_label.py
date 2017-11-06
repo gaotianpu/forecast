6a379754cc56036cce095c62b9333cc5f35d62c8
gaotianpu <gaotianpu@gmail.com>
2017-11-06 12:12:59 +0800

    cc

--- v3/bin/make_label.py
+++ v3/bin/make_label.py
@@ -8,7 +8,7 @@ close=0的情况？
 """
 import os
 import sys
-import numpy as np 
+import numpy as np
 import common
 import stock_meta
 
@@ -43,80 +43,86 @@ def get_next(rows, index, days=3, key='close'):
 
     return ret
 
-def get_stat(rows,key):
+
+def get_stat(rows, key):
     """获得 均值，标准差
     http://www.cnblogs.com/smallpi/p/4550361.html
-    """ 
-    arr = np.array([float(row[key]) for row in last_rows])
-    ret['last_%s_max1' % key] = arr.max()
-    ret['last_%s_min1' % key] = arr.min()
+    """
+    ret = {}
+    arr = np.array([float(row[key]) for row in rows])
+    ret['last_%s_max' % key] = arr.max()
+    ret['last_%s_min' % key] = arr.min()
     ret['last_%s_mean' % key] = arr.mean()
     ret['last_%s_std' % key] = arr.std()
-    ret['last_%s_median' % key] = np.median(arr) #.median()
-    return ret 
+    ret['last_%s_median' % key] = np.median(arr)  # .median()
+    return ret
+
 
 def get_last(rows, index, days=10):
     """过去days天内表现，ML的feather部分"""
     # 过去10个交易日的统计指标，均值，标准差，max(high)，min(low)，
     # open,close,high,low,成交量，换手率等，排除=0的情况
     # last_rows = rows[index:index+days]
-    last_rows = [row for row in rows[index:index + days] if float(row['close']) > 0]
-    
-    last_one = last_rows[-1] #最远的一个？
+    ret = {}
+
+    last_rows = [row for row in rows[index:index + days]
+                 if float(row['close']) > 0]
+    if not last_rows:
+        return ret
+
+    # max,min
+    ret['last_high_max'] = max([row['high'] for row in last_rows])
+    ret['last_low_min'] = min([row['low'] for row in last_rows])
+
+    ret_close = get_stat(last_rows, "close")
+    ret_turn_over = get_stat(last_rows, "turn_over")
+    ret_vo_turn_over = get_stat(last_rows, "vo_turn_over")
+
+    ret = dict(ret.items() + ret_close.items() +
+               ret_turn_over.items() + ret_vo_turn_over.items())
+
     current = rows[index]
-    
+    last_one = last_rows[-1]  # 最远的一个？
 
     # 再当前值的z-score ?
-    ret = {}
+
     ret['last_10_open'] = last_one['open']
-    if last_one['open'] :
-        ret['last_10_rate'] = (float(current['open']) - float(last_one['open']))/ float(last_one['open'])
+    if last_one['open']:
+        ret['last_10_rate'] = (float(current['open']) -
+                               float(last_one['open'])) / float(last_one['open'])
 
-    #max,min
-    ret['last_%s_max' % days] = None
-    ret['last_%s_min' % days] = None 
-    if last_rows:
-        ret['last_high_max' % days] = max([row['high'] for row in last_rows])
-        ret['last_low_min' % days] = min([row['low'] for row in last_rows])
+    # ret['id'] = current['id']
 
-        ret_close = get_stat(last_rows,"close")
-        ret_turn_over = get_stat(last_rows,"turn_over")
-        ret_vo_turn_over = get_stat(last_rows,"vo_turn_over")   
-    
-    current = rows[index]
-    ret['id'] = current['id']
-    print ret
+    return ret
 
 
 def rolling(rows, index):
-    current = rows[index]
+    """处理单个stock"""
+    current = rows[index] 
 
-    ret = get_next(rows, index, 3, "close")
+    # gen for label
+    ret1 = get_next(rows, index, 3, "close")
+
+    # gen for features
     ret2 = get_last(rows, index)
 
+    ret = dict(ret1.items() + ret2.items())
     ret['id'] = current['id']
-    # print ret
 
-    # forcast = li[i - days]
-    # CHG = round(float(forcast[key]) - float(current[key]), 2) #涨跌额
-    # PCHG = round(100 * CHG / float(current[key]), 1) #涨跌幅
-
-    # # print forcast[0], current[key], forcast[key]
-    # ret = (current[0],current[1], current[3],forcast[1],CHG, PCHG)
-    # print ','.join( str(x) for x in ret )
-
-    # return (current[0], current[2], CHG, PCHG)
+    return ret
 
 
 def process_stock(stock_no, days):
+    """处理单个stock"""
     csv_file = "%s/convert_%s.csv" % (conf.HISTORY_CONVERTED_PATH, stock_no)
     rows = common.load_csv(csv_file, fields)
     rows = list(rows)
     for i, row in enumerate(rows):
-        rolling(rows, i)
+        print rolling(rows, i)
 
 
 def main(days):
+    """主函数，处理所有stocks"""
     stocks = stock_meta.load_all()
     for stock in stocks:
         process_stock(stock['stock_no'], days)
